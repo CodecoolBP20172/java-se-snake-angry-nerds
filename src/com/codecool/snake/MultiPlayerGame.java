dc061cbe119c933442476db49dfdb1f6c1ac98f5
mateszummer <szummerm@gmail.com>
2017-11-29 21:47:23 +0100

    Merge branch 'development' into 2_gameover
    # Conflicts:
    #	src/com/codecool/snake/Game.java
    #	src/com/codecool/snake/MultiPlayerGame.java

--- src/com/codecool/snake/MultiPlayerGame.java
+++ src/com/codecool/snake/MultiPlayerGame.java
@@ -7,7 +7,8 @@ public class MultiPlayerGame extends Game {
     public MultiPlayerGame(int numberOfSnakes){
         super();
         for (int i = 1; i < numberOfSnakes; i++) {
-            snakeHeads.add(new SnakeHead(this, i*200, 650, Globals.keyControls.get(i)));
+            HealthBar healthBar = new HealthBar(this, String.format("P%d: ", i+1), 30,60 + i*40);
+            snakeHeads.add(new SnakeHead(this, i*200, 650, Globals.keyControls.get(i), healthBar));
         }
     }
 }
