e00884b763a1c4a158933c5e8f4e88664ed2adcc
Ollári Márton <ollarimarton@gmail.com>
2017-11-29 14:37:23 +0100

    snakes cant collide

--- src/com/codecool/snake/entities/snakes/SnakeHead.java
+++ src/com/codecool/snake/entities/snakes/SnakeHead.java
@@ -17,7 +17,7 @@ public class SnakeHead extends GameEntity implements Animatable {
     private static final float speed = 2;
     private static final float turnRate = 2;
     private GameEntity tail; // the last element. Needed to know where to add the next part.
-    private static List<SnakeBody> bodyParts = new ArrayList<>();
+    private List<SnakeBody> bodyParts = new ArrayList<>();
     private int health;
     private KeyControl keyControl;
     private boolean isAlive;
@@ -57,15 +57,23 @@ public class SnakeHead extends GameEntity implements Animatable {
             if (getBoundsInParent().intersects(entity.getBoundsInParent())) {
                 if (entity instanceof Interactable) {
                     Interactable interactable = (Interactable) entity;
-                    interactable.apply(this);
-                    System.out.println(interactable.getMessage());
+                    if (snakeIntersects(entity)) {
+                        interactable.apply(this);
+                        System.out.println(interactable.getMessage());
+                    }
                 }
-                else if (entity instanceof SnakeBody && ((SnakeBody) entity).getSnakeHead() != this){
-                    if (snakeIntersects((SnakeBody) entity)){
-                        health = 0;
-                    };
-//                    entity instanceof SnakeHead && entity != this
+                else if (entity instanceof SnakeBody){
+                    if (this.bodyPartsToAvoid().contains(entity) || ((SnakeBody) entity).getSnakeHead() != this){
+                        if (snakeIntersects(entity)) {
+                            health = 0;
+                        }
+                    }
                 }
+                else if (entity instanceof SnakeHead && entity != this){
+                        if (snakeIntersects(entity)){
+                            health = 0;
+                        }
+                    }
             }
         }
 
@@ -90,10 +98,10 @@ public class SnakeHead extends GameEntity implements Animatable {
         return bodyParts;
     }
 
-    public List<SnakeBody> getBodyPartsBySnakeHead(SnakeHead head){
-        
-        return bodyParts;
+    public List<SnakeBody> bodyPartsToAvoid(){
+        return bodyParts.subList(4, bodyParts.size());
     }
+
 
     public void changeHealth(int diff) {
         health += diff;
@@ -103,7 +111,7 @@ public class SnakeHead extends GameEntity implements Animatable {
         return isAlive;
     }
 
-    private boolean snakeIntersects(SnakeBody b) {
+    private boolean snakeIntersects(GameEntity b) {
         Bounds headBounds = getBoundsInParent();
         Bounds bodyBounds = b.getBoundsInParent();
         Point2D headCenter = new Point2D((headBounds.getMaxX() + headBounds.getMinX()) * 0.5,
@@ -111,10 +119,6 @@ public class SnakeHead extends GameEntity implements Animatable {
         Point2D bodyCenter = new Point2D((bodyBounds.getMaxX() + bodyBounds.getMinX()) * 0.5,
                 (bodyBounds.getMaxY() + bodyBounds.getMinY()) * 0.5);
         final double radius = (bodyBounds.getMaxX() - bodyBounds.getMinX()) * 0.5;
-        /**
-         * Subtraction gives us a vector pointing from the body to the head.
-         * Magnitude gives us the length of this vector (distance of the two points).
-         */
         return headCenter.subtract(bodyCenter).magnitude() < 2.0 * radius;
     }
 }
